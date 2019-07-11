075e8c0bea1511dabfb243ca5583fd1854943e26
Vinton Teo <taulago@ymail.com>
2019-07-11 10:48:08 -0600

    Create methods to add and cancel reservations.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import ResyForm from './ResyForm.js';
+import ResyContainer from './ResyContainer.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    };
+  }
+
+  componentDidMount = () => {
+    //
+  };
+
+  addRes = newRes => {
+    const { reservations } = this.state;
+    this.setState({ reservations: [...reservations, newRes] });
+  };
+
+  cancelRes = id => {
+    const { reservations } = this.state;
+    const filteredRes = reservations.filter(res => res.id !== id);
+    this.setState({ reservations: filteredRes });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <ResyForm addRes={this.addRes} />
+        <ResyContainer
+          data={this.state.reservations}
+          cancelRes={this.cancelRes}
+        />
       </div>
-    )
+    );
   }
 }
 
